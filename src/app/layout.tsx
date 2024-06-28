367e11c768771793f701819a22434cee85152f69
Schleidens <alectineschleidens2016@gmail.com>
2024-06-27 23:15:55 -0400

    update SEO details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,10 +9,29 @@ export const revalidate = 30;
 
 const inter = Inter({ subsets: ['latin'] });
 
+const url = new URL('https://www.schleidens.dev/');
+
 export const metadata: Metadata = {
+  icons: {
+    icon: ['/public/favicon.ico'],
+    apple: ['/public/apple-touch-icon.png?v=4'],
+    shortcut: ['/public/apple-touch-icon.png'],
+  },
   title: 'Alectine Schleidens - Fullstack Software Developer',
   description:
-    "I'm an experienced Full Stack Software Developer with 5+ years of experience",
+    'I am a software developer specializing in building user-friendly web-apps. Problem solver, calm under pressure, quick learner with the goal of customer satisfaction.',
+  metadataBase: url,
+  openGraph: {
+    title: 'Alectine Schleidens - Fullstack Software Developer',
+    description:
+      'I am a software developer specializing in building user-friendly web-apps. Problem solver, calm under pressure, quick learner with the goal of customer satisfaction.',
+    siteName: 'Alectine Schleidens - Fullstack Software Developer',
+    url: url,
+    type: 'website',
+    images: 'https://www.schleidens.dev/Alectine_Schleidens.jpg',
+  },
+  keywords:
+    'Schleidens, Schleiden, Schlei, Dens, Schlei Dens, Alectine, Alextine, Alec, Alex, Tchway, Try',
 };
 
 export default function RootLayout({
